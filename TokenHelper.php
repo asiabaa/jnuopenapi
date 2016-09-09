320fe975e8527bd558b8d9f16bdcd810085ac7ae
jack <asiabaa@gmail.com>
2016-09-09 11:55:31 +0800

    fix

--- TokenHelper.php
+++ TokenHelper.php
@@ -9,7 +9,7 @@ class TokenHelper{
 
 	public static function getToken($app_id,$app_secret,$api_url){
 
-		$token = Yii::app()->params['openapi_token'];
+		$token = Yii::$app->params['openapi_token'];
 		if($token){
 			if(time() - $token['time'] < 2 * 60 * 55 * 1000){
 				return $token;
@@ -18,20 +18,27 @@ class TokenHelper{
 
 		$now = time();
 		$curl = new Curl();
+		$token = array();
 		$resp = $curl->get($api_url . '?appid=$app_id&$appsecret=$app_secret');
-		$json = json_decode($resp['body']);
-		if($json){
-			if($json['result'] == 200){
-				$token = array(
-					'token' => $json['data'],
-					'time' => $now,
-				);
-				Yii::app()->params['openapi_token'] = $token;
-				return $token;
+		if($resp['code'] == 200){
+			$json = json_decode($resp['body']);
+			if($json){
+				if($json['result'] == 200){
+					$token = array(
+						'token' => $json['data'],
+						'time' => $now,
+					);
+					Yii::$app->params['openapi_token'] = $token;
+					return $token;
+				}else{
+					$token['error'] = 'app_secret认证错误';
+				}
 			}
+		}else{
+			$token['error'] = '网络错误';
 		}
 
-		return null;
+		return $token;
 	}
 
 }
